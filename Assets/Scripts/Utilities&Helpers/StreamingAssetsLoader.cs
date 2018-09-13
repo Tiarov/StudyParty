74b86a94bc21a144d7055df850a3195a330d5d4d
Tiarov <tiarov@gmail.com>
2018-09-13 17:08:21 +0300

    Read and parse json from StreamingAssetsPath

---
+++ Assets/Scripts/Utilities&Helpers/StreamingAssetsLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class StreamingAssetsLoader : MonoBehaviour
+{
+    public string FileName = "GameObjectsInfo.json";
+
+    private void Start()
+    {
+        var json = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, FileName));
+        var infos = JsonUtility.FromJson<InfosCollection>(json).ObjectsList;
+
+        foreach (var info in infos)
+        {
+            Debug.Log(info.Name + " " + info.Position[0]);
+        }
+    }
+}
