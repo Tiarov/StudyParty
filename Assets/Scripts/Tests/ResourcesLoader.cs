d5409a0733ebed66f7cac1128922f8b8646f6a9c
Tiarov <tiarov@gmail.com>
2018-09-14 12:24:18 +0300

    realization of About resources task

---
+++ Assets/Scripts/Tests/ResourcesLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class ResourcesLoader : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(Method());
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private IEnumerator Method()
+    {
+        var prefab1 = Resources.Load<GameObject>("Cube3");
+        var prefab2 = Resources.Load<GameObject>("Cube2");
+
+        yield return new WaitForSeconds(1);
+
+        var go1 = Instantiate(prefab1);
+        var go2 = Instantiate(prefab2);
+
+        yield return new WaitForSeconds(1);
+
+        Destroy(go1);
+        Destroy(go2);
+
+        yield return new WaitForSeconds(1);
+       
+        Resources.UnloadUnusedAssets();
+    }
+}
