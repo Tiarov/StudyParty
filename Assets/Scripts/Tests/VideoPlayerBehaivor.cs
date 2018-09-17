d1ee10900ac261aa8b5ab7d1cb21193dd5d3a476
Tiarov <tiarov@gmail.com>
2018-09-17 15:01:10 +0300

    Vuforia SDK task. Create simple app about video on targets

---
+++ Assets/Scripts/Tests/VideoPlayerBehaivor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class VideoPlayerBehaivor : MonoBehaviour
+{
+    public Material VideoMaterial;
+    public MovieTexture Clip;
+
+    public VuforiaExtensionEventHandler Events;
+
+    [SerializeField, Range(0, 1)]
+    private float Volume = 0.2f;
+
+    private AudioSource _audioSource;
+    private MovieTexture _video;
+
+    private void Start()
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.material = new Material(VideoMaterial);
+        VideoMaterial = renderer.material;
+
+        VideoMaterial.mainTexture = Clip;
+        _video = (MovieTexture)VideoMaterial.mainTexture;
+
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = _video.audioClip;
+
+        Events.TrackingFoundEvent += OnTrackingFound;
+        Events.TrackingLostEvent += OnTrackingLost;
+        OnTrackingFound();
+    }
+
+    private void OnDestroy()
+    {
+        Events.TrackingFoundEvent -= OnTrackingFound;
+        Events.TrackingLostEvent -= OnTrackingLost;
+    }
+
+    public void OnTrackingLost()
+    {
+        _video.Stop();
+        _audioSource.Stop();
+    }
+
+    public void OnTrackingFound()
+    {
+        _video.Play();
+        _audioSource.Play();
+        _audioSource.volume = Volume;
+    }
+}
---
+++ Assets/Scripts/Tests/VuforiaExtensionEventHandler.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+using Vuforia;
+
+public class VuforiaExtensionEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    public TrackableBehaviour MTrackableBehaviour;
+
+    public System.Action TrackingFoundEvent;
+    public System.Action TrackingLostEvent;
+
+    private void Start()
+    {
+        if (MTrackableBehaviour)
+        {
+            MTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+
+    public void OnTrackingLost()
+    {
+        if (TrackingLostEvent != null)
+            TrackingLostEvent();
+    }
+
+    public void OnTrackingFound()
+    {
+        if (TrackingFoundEvent != null)
+            TrackingFoundEvent();
+    }
+
+}
